0bf554be38332aafcb0f6536044e5bdda7d570c6
Shelikhoo <xiaokangwang@outlook.com>
2017-07-19 17:24:32 +0800

    Finalize Tool

--- main.go
+++ main.go
@@ -1,5 +1,105 @@
 package main
 
-func main() {
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
 
+	vlencoding "github.com/xiaokangwang/V2RayConfigureFileUtil/encoding"
+
+	qrcode "github.com/skip2/go-qrcode"
+)
+
+func main() {
+	var target = flag.String("t", "QR", "output target")
+	var input = flag.String("i", "i.json", "input file/dir")
+	var output = flag.String("o", "o", "output file/dir")
+	flag.Parse()
+	switch *target {
+	case "QR":
+		qr(*input, *output)
+	case "seg":
+		seg(*input, *output)
+	case "rseg":
+		//case "fpb":
+
+	}
+}
+
+func seg(input, output string) {
+	ec := &vlencoding.Encoder{}
+	qrconf := &vlencoding.QRGenConf{}
+	qrconf.ForceReconstruct = true
+	qrconf.MaxQrSize = 1024
+	qrconf.ReconsConf = &vlencoding.QRGenConf_ReconstructConf{AtLeastMutiply: 2.0, AtLeastReplacement: 4}
+	data, err := ec.PackV2RayConfigureIntoPackedForm(input)
+	if err != nil {
+		panic(err)
+	}
+	qrd, err := ec.EncodeToL2QR(data, *qrconf)
+	if err != nil {
+		panic(err)
+	}
+	for currOutput := 0; currOutput < qrd.TotalSegment; currOutput++ {
+		qd, err := qrd.GetSegmentAsByte(currOutput)
+		if err != nil {
+			panic(err)
+		}
+		err = ioutil.WriteFile(fmt.Sprintf("%v/%v.seg.libv2ray.pb", output, currOutput), qd, 0600)
+		if err != nil {
+			panic(err)
+		}
+	}
+}
+
+func qr(input, output string) {
+	ec := &vlencoding.Encoder{}
+	payload, err := ioutil.ReadFile(input)
+	if err != nil {
+		panic(err)
+	}
+	url := ec.ByteToV2RayURL(payload)
+	var png []byte
+	png, err = qrcode.Encode(url, qrcode.Medium, 512)
+	if err != nil {
+		panic(err)
+	}
+	ioutil.WriteFile(output, png, 0600)
+}
+
+func rseg(input, output string) {
+	ec := &vlencoding.Encoder{}
+	qd := ec.StartQRDecode()
+	filepath.Walk(input, func(path string, info os.FileInfo, err error) error {
+		if !info.IsDir() {
+			payload, err := ioutil.ReadFile(input)
+			if err != nil {
+				fmt.Println(err)
+			}
+			err = qd.OnNewDataScanned(payload)
+			if err != nil {
+				fmt.Println(err)
+			}
+		}
+		return nil
+	})
+	if !qd.IsDecodeReady() {
+		panic(fmt.Sprintf("Decode not ready %v / %v", qd.PieceReceived, qd.PieceNeeded))
+	}
+	out, err := qd.Finish()
+	if err != nil {
+		panic(err)
+	}
+	//Now unwrap file
+	ext, pay, err := ec.UnpackV2RayConfB(out)
+	if err != nil {
+		panic(err)
+	}
+	//WriteFile
+	err = ioutil.WriteFile(output+ext, pay, 0600)
+	if err != nil {
+		panic(err)
+	}
 }
