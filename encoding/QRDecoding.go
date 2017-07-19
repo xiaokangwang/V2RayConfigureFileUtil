a5f9846b45831d539793f02d05cfd5a2bd7d474d
Shelikhoo <xiaokangwang@outlook.com>
2017-07-19 11:32:56 +0800

    QR Code Decoding

---
+++ encoding/QRDecoding.go
@@ -0,0 +1,91 @@
+package encoding
+
+import (
+	"bytes"
+	"errors"
+
+	"github.com/golang/protobuf/proto"
+	"github.com/klauspost/reedsolomon"
+	"golang.org/x/crypto/sha3"
+)
+
+type QRDecoder struct {
+	notfirstQR    bool
+	firstMessage  *LibV2RayQRCode
+	payload       [][]byte
+	PieceNeeded   int
+	PieceReceived int
+}
+
+func (e *Encoder) StartQRDecode() *QRDecoder {
+	return &QRDecoder{}
+}
+
+func (qd *QRDecoder) OnNewDataScanned(data []byte) error {
+	newdata := new(LibV2RayQRCode)
+	err := proto.Unmarshal(data, newdata)
+	if err != nil {
+		return err
+	}
+	//Is this first QR being progressed? if so, do init
+	if !qd.notfirstQR {
+		qd.memalloc(newdata)
+		qd.notfirstQR = true
+		qd.firstMessage = newdata
+		qd.PieceNeeded = int(newdata.OriginalSegmentNum)
+	} else {
+		//Or Check if is same message
+		if !bytes.Equal(newdata.MsgChecksum, qd.firstMessage.MsgChecksum) {
+			return errors.New("inconsistent message group")
+		}
+	}
+	//Check if payload Checksum is correct
+	PayloadCheckSum := sha3.Sum256(newdata.Payload)
+
+	if bytes.Equal(PayloadCheckSum[:], newdata.PayloadChecksum) {
+		return errors.New("Bad Checksum")
+	}
+	//Assign payload if of differnet Message Number
+	if qd.payload[newdata.MessgaeNumber] == nil {
+		qd.payload[newdata.MessgaeNumber] = newdata.Payload
+		//Update Stat
+		qd.PieceReceived += 1
+	}
+	return nil
+}
+
+func (qd *QRDecoder) memalloc(firstQR *LibV2RayQRCode) {
+	//Check if it is only consist of one segment
+	if firstQR.MessageSegType == LibV2RayQRCode_NoSegmention {
+		//No additional alloc is needed, as there is only one message
+		qd.payload = make([][]byte, 1)
+	} else {
+		qd.payload = make([][]byte, firstQR.GroupCount)
+	}
+}
+
+func (qd *QRDecoder) IsDecodeReady() bool {
+	return qd.PieceNeeded >= qd.PieceReceived
+}
+
+func (qd *QRDecoder) Finish() ([]byte, error) {
+	if !qd.IsDecodeReady() {
+		return nil, errors.New("Not Ready")
+	}
+	//if there is only one piece, just return it
+	if qd.firstMessage.MessageSegType == LibV2RayQRCode_NoSegmention {
+		return qd.payload[0], nil
+	}
+	//then we do the reconstruction
+	enc, err := reedsolomon.New(int(qd.firstMessage.OriginalSegmentNum), int(qd.firstMessage.GroupCount-qd.firstMessage.OriginalSegmentNum))
+	if err != nil {
+		return nil, err
+	}
+	err = enc.Reconstruct(qd.payload)
+	if err != nil {
+		return nil, err
+	}
+	var bytebuf bytes.Buffer
+	enc.Join(&bytebuf, qd.payload, int(qd.firstMessage.PayloadTotalSize))
+	return bytebuf.Bytes(), nil
+}
--- encoding/QREncoding.go
+++ encoding/QREncoding.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"math"
 
+	"github.com/golang/protobuf/proto"
 	"github.com/klauspost/reedsolomon"
 
 	"golang.org/x/crypto/sha3"
@@ -13,12 +14,13 @@ func (e *Encoder) EncodeToL2QR(payload []byte, conf QRGenConf) (*L2QRGen, error)
 	//First Check the size of data being encoded
 	size := len(payload)
 	Gener := new(L2QRGen)
+	Gener.templ.PayloadTotalSize = int32(size)
 	if size < int(conf.MaxQrSize) && !conf.ForceReconstruct {
 		Gener.TotalSegment = 1
+		Gener.templ.OriginalSegmentNum = 1
 		Gener.templ.GroupCount = 1
 		Gener.templ.MessageSegType = LibV2RayQRCode_NoSegmention
 		Gener.payload = make([][]byte, 1)
-		Gener.templ.PayloadTotalSize = int32(size)
 		Gener.payload[0] = payload
 	} else {
 		Gener.PayloadSegment = size / int(conf.MaxQrSize)
@@ -26,37 +28,45 @@ func (e *Encoder) EncodeToL2QR(payload []byte, conf QRGenConf) (*L2QRGen, error)
 		Gener.TotalSegment = Gener.ReconstructSegment + Gener.PayloadSegment
 		Gener.templ.GroupCount = int32(Gener.TotalSegment)
 		Gener.templ.MessageSegType = LibV2RayQRCode_Reconstruct
-		Gener.payload = make([][]byte, Gener.TotalSegment)
-		BytePerSegment := int(math.Floor(float64(size)/float64(Gener.PayloadSegment)) + 1)
-		Gener.templ.ReconstructPadTo = int32(BytePerSegment)
+		//Gener.payload = make([][]byte, Gener.TotalSegment)
+		//BytePerSegment := int(math.Floor(float64(size)/float64(Gener.PayloadSegment)) + 1)
+		//Gener.templ.ReconstructPadTo = int32(BytePerSegment)
 		Gener.templ.OriginalSegmentNum = int32(Gener.PayloadSegment)
-		//Copy First Few Segments
+		/*
+			//Copy First Few Segments
 
-		for CurrentLinking := 0; CurrentLinking < Gener.PayloadSegment-1; CurrentLinking++ {
-			if CurrentLinking == Gener.PayloadSegment-1 {
-				//First find out if input data is even to payload
-				RecSize := Gener.PayloadSegment * BytePerSegment
-				padding := RecSize - size
-				lseg := BytePerSegment - padding
-				if padding == 0 {
-					Gener.payload[CurrentLinking] = payload[CurrentLinking*BytePerSegment : (CurrentLinking+1)*BytePerSegment-1]
+			for CurrentLinking := 0; CurrentLinking < Gener.PayloadSegment-1; CurrentLinking++ {
+				if CurrentLinking == Gener.PayloadSegment-1 {
+					//First find out if input data is even to payload
+					RecSize := Gener.PayloadSegment * BytePerSegment
+					padding := RecSize - size
+					lseg := BytePerSegment - padding
+					if padding == 0 {
+						Gener.payload[CurrentLinking] = payload[CurrentLinking*BytePerSegment : (CurrentLinking+1)*BytePerSegment-1]
+					} else {
+						Gener.payload[CurrentLinking] = make([]byte, BytePerSegment)
+						copy(Gener.payload[CurrentLinking][0:lseg], payload[CurrentLinking*BytePerSegment:(CurrentLinking+1)*BytePerSegment-1-padding])
+					}
 				} else {
-					Gener.payload[CurrentLinking] = make([]byte, BytePerSegment)
-					copy(Gener.payload[CurrentLinking][0:lseg], payload[CurrentLinking*BytePerSegment:(CurrentLinking+1)*BytePerSegment-1-padding])
+					Gener.payload[CurrentLinking] = payload[CurrentLinking*BytePerSegment : (CurrentLinking+1)*BytePerSegment-1]
 				}
-			} else {
-				Gener.payload[CurrentLinking] = payload[CurrentLinking*BytePerSegment : (CurrentLinking+1)*BytePerSegment-1]
 			}
-		}
 
-		//alloc memory for shards
+			//alloc memory for shards
 
-		for CurrentAllocShard := 0; CurrentAllocShard < Gener.ReconstructSegment; CurrentAllocShard++ {
-			Gener.payload[Gener.PayloadSegment+CurrentAllocShard] = make([]byte, BytePerSegment)
-		}
-
+			for CurrentAllocShard := 0; CurrentAllocShard < Gener.ReconstructSegment; CurrentAllocShard++ {
+				Gener.payload[Gener.PayloadSegment+CurrentAllocShard] = make([]byte, BytePerSegment)
+			}
+		*/
 		//now calc reconstruction
 		enc, err := reedsolomon.New(Gener.PayloadSegment, Gener.ReconstructSegment)
+		if err != nil {
+			return nil, err
+		}
+		Gener.payload, err = enc.Split(payload)
+		if err != nil {
+			return nil, err
+		}
 		enc.Encode(Gener.payload)
 		if err != nil {
 			return nil, err
@@ -84,4 +94,13 @@ type L2QRGen struct {
 	TotalSegment       int
 	templ              LibV2RayQRCode
 	payload            [][]byte
+}
+
+func (qg *L2QRGen) GetSegmentAsByte(seg int) ([]byte, error) {
+	objectPayload := qg.payload[seg]
+	PayloadCheckSum := sha3.Sum256(objectPayload)
+	qg.templ.Payload = objectPayload
+	qg.templ.PayloadChecksum = PayloadCheckSum[:]
+	qg.templ.MessgaeNumber = int32(seg)
+	return proto.Marshal(&qg.templ)
 }
